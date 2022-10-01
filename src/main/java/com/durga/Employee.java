61390a4a2ed4e482d284a712b9bf89d5bee363a7
Durga <dpjakkamsetti@gmail.com>
2022-10-01 18:59:51 +0530

    Employee v2

--- src/main/java/com/durga/Employee.java
+++ src/main/java/com/durga/Employee.java
@@ -1,4 +1,23 @@
 package com.durga;
 
 public class Employee {
+
+    private String empName;
+    private String location;
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
